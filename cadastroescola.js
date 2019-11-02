65e301488d8840a0287b4e65f3e472ea550da42c
leyla-andrade <57011909+leyla-andrade@users.noreply.github.com>
2019-11-02 17:15:00 -0300

    arquivo13

--- cadastroescola.js
+++ cadastroescola.js
@@ -1,34 +1,34 @@
-const buttonSingUp = document.querySelector('button[name=signup]');
-const buttonSingUp = document.querySelector('button[name=signup]');
+const buttonSignUp = document.querySelector('button[name=signup]');
+const buttonSignIn = document.querySelector('button[name=signin]');
 
 const inputEmail = document.querySelector('input[name=email]');
 const inputPassword = document.querySelector('input[name=password]');
 
 const firebaseAuth = firebase.auth();
 
-buttonSignUp.addEventListener('click', () =>{
-    firebaseAuth.createUserWithEmailAndPassword(inputEmail.Value, inputPassword.value )
+buttonSignUp.addEventListener('click', () => {
+    firebaseAuth.createUserWithEmailAndPassword(inputEmail.value, inputPassword.value)
         .then(result => {
-         alert('Conta feita com sucesso');
-         console.log(result);
-         const user = result.user;
-         user.sendEmailVerification().then(result => {
-             alert('E-mail enviado com sucesso!');
-         }).catch(err => {
-             alert('Verifique seu e-mail, não foi enviado!');
-         })
-        }) .catch(err => {
+            alert('Conta feita com sucesso');
+            console.log(result);
+            const user = result.user;
+            user.sendEmailVerification().then(result => {
+                alert('E-mail enviado com sucesso!');
+            }).catch(err => {
+                alert('Verifique seu e-mail, não foi enviado!');
+            })
+        }).catch(err => {
             alert('Ocorreu um Erro, tente novamente!')
             console.log(err);
         })
-    
+
 });
 
-buttonSignUp.addEventListener('click', () => {
+buttonSignIn.addEventListener('click', () => {
     firebaseAuth.signWithEmailAndPassword(inputEmail.value, inputPassword.value)
-       .then(result => {
-          console.log(result); 
-       }).catch(err => {
-           console.log(err);
-       })
+        .then(result => {
+            console.log(result);
+        }).catch(err => {
+            console.log(err);
+        })
 });
