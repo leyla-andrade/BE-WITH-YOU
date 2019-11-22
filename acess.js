4b75e37546558691a1b88f612e0c3d5f41dedef6
giseleLucena <57012300+giseleLucena@users.noreply.github.com>
2019-11-22 14:05:50 -0300

    ardr

---
+++ acess.js
@@ -0,0 +1,17 @@
+< script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" > < /script> <
+    script src = "" > < /script>
+
+<
+button type = "button"
+id = "btnAumentar" > Aumentar fonte < /button> <
+    button type = "button"
+id = "btnDiminuir" > Diminuir fonte < /button>
+
+<
+div id = "content" >
+    <
+    h3 > Neque porro quisquam < /h3> <
+    p > Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... <
+    a > Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... < /a> <
+    /p> <
+    /div>
