0ff2c5eb2a83a5ba6fbe3cc140082b623821eb35
samuelkepler <57400752+samuelkepler@users.noreply.github.com>
2019-11-26 11:12:49 -0300

    arquivo40

--- cadastroescola.js
+++ cadastroescola.js
@@ -4,19 +4,19 @@ const firebaseAuth = firebase.auth();
 buttonSign.addEventListener('click', () => {
     const inputEmail = document.getElementById('emailInput').value;
     const inputPassword = document.getElementById('passwordInput').value;
-    
+
     firebaseAuth.createUserWithEmailAndPassword(inputEmail, inputPassword)
-    .then(result => {
-            alert('Conta feita com sucesso');
+        .then(result => {
+            alert('Conta feita com sucesso!! Faça seu login e confira nossas ferramentas');
             console.log(result);
             const user = result.user;
             user.sendEmailVerification().then(result => {
-                alert('E-mail enviado com sucesso!');
+                alert('E-mail de verificação de usuário enviado com sucesso!');
             }).catch(err => {
-                alert('Verifique seu e-mail, não foi enviado!');
+                alert('Verifique seu e-mail, não foi enviado um e-mail de veficação do usuário!');
             })
         }).catch(err => {
-            alert('Ocorreu um Erro, tente novamente!')
+            alert('Ocorreu um Erro, tente novamente!');
             console.log(err);
         })
 
--- dataformu.js
+++ dataformu.js
@@ -1,18 +1,18 @@
 let nomeInput = document.getElementById('nomeInput');
 let dataInput = document.getElementById('dataInput');
-let ameacasInput = document.getElementById('ameacasInput');
-let assedioMoralInput = document.getElementById('assedioMorallInput');
-let assedioSexualInput = document.getElementById('assedioSexualInput');
-let boatosInput = document.getElementById('boatosInput');
-let brigasInput = document.getElementById('brigasInput');
-let bullyingInput = document.getElementById('bullyingInput');
-let isolamentosInput = document.getElementById('isolamentosInput');
-let professoresInput = document.getElementById('professoresInput');
-let cozinheirosInput = document.getElementById('cozinheirosInput');
-let segurancasInput = document.getElementById('segurancasInput');
-let gestorInput = document.getElementById('gestorInput');
-let alunosInput = document.getElementById('alunosInput');
-let admInput = document.getElementById('admInput');
+// let ameacasInput = document.getElementById('ameacasInput');
+// let assedioMoralInput = document.getElementById('assedioMorallInput');
+// let assedioSexualInput = document.getElementById('assedioSexualInput');
+// let boatosInput = document.getElementById('boatosInput');
+// let brigasInput = document.getElementById('brigasInput');
+// let bullyingCheckbox = document.getElementById('bullyingInput');
+// let isolamentosCheckbox = document.getElementById('isolamentosInput');
+// let professoresCheckbox = document.getElementById('professoresInput');
+// let cozinheirosCheckbox = document.getElementById('cozinheirosInput');
+// let segurancasCheckbox = document.getElementById('segurancasInput');
+// let gestorCheckbox = document.getElementById('gestorInput');
+// let alunosCheckbox = document.getElementById('alunosInput');
+// let admCheckbox = document.getElementById('admInput');
 let perguntaInput = document.getElementById('perguntaInput');
 let perguntadInput = document.getElementById('perguntadInput');
 let perguntatInput = document.getElementById('perguntatInput');
@@ -21,37 +21,38 @@ let perguntacInput = document.getElementById('perguntacInput');
 let perguntasInput = document.getElementById('perguntasInput');
 let perguntaseInput = document.getElementById('perguntaseInput');
 let adcButton = document.getElementById('adcButton');
+const buttonSign = document.querySelector('button[name=adcButton]');
 
 adcButton.addEventListener('click', function (){
-    create(nomeInput, dataInput, ameacasInput, assedioMoralInput, assedioSexualInput, boatosInput, brigasInput, bullyingInput, isolamentosInput, professoresInput, cozinheirosInput, segurancasInput, gestorInput, alunosInput, admInput, perguntaInput, perguntadInput, perguntatInput, perguntatInput, perguntaqInput, perguntacInput, perguntasInput, perguntaseInput)
+    create(nomeInput.value, dataInput.value, perguntaInput.value, perguntadInput.value, perguntatInput.value, perguntatInput.value, perguntaqInput.value, perguntacInput.value, perguntasInput.value, perguntaseInput.value)
 });
 
 function create(nome, data, ameacas, assedioMoral, assedioSexual, boatos, brigas, bullying, isolamentos,  professores, cozinheiros, segurancas, gestor, alunos, adm, pergunta, perguntad, perguntat, perguntaq, perguntac, perguntas, perguntase){
     let user = {
         nome: nome,
         data: data,
-        ameacas: ameacas,
-        assedioMoral: assedioMoral,
-        assedioSexual : assedioSexual,
-        boatos : boatos,
-        brigas : brigas,
-        bullying : bullying,
-        isolamentos : isolamentos,
-        professores : professores,
-        cozinheiros : cozinheiros,
-        segurancas : segurancas,
-        gestor : gestor,
-        alunos : alunos,
-        adm : adm,
+        // ameacas: ameacas,
+        // assedioMoral: assedioMoral,
+        // assedioSexual : assedioSexual,
+        // boatos : boatos,
+        // brigas : brigas,
+        // bullying : bullying,
+        // isolamentos : isolamentos,
+        // professores : professores,
+        // cozinheiros : cozinheiros,
+        // segurancas : segurancas,
+        // gestor : gestor,
+        // alunos : alunos,
+        // adm : adm,
         pergunta : pergunta,
         perguntad : perguntad,
         perguntat : perguntat,
         perguntaq : perguntaq,
         perguntac : perguntac,
         perguntas : perguntas,
-        perguntase : pergunta,
+        perguntase : perguntase,
     };
-    return firebase.database().ref().child('Questionário').push(user);
+    return firebase.database().ref().child('questionario').push(user);
 }
 
 
