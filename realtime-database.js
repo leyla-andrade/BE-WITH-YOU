4b9a7ea79c39a36fd45d7282b930280c921fda77
leyla-andrade <leylaluizyramos@gmail.com>
2019-12-02 00:10:48 -0300

    arquivo48

--- realtime-database.js
+++ realtime-database.js
@@ -1,20 +1,5 @@
-const nameInput = document.getElementById('nomeInput');
-const dataInput = document.getElementById('dataInput');
-const textInput = document.getElementById('textInput');
-const exemplosInput = document.getElementById('exemplosInput');
-const perguntaInput = document.getElementById('perguntaInput');
-const perguntadInput = document.getElementById('perguntadInput');
-const perguntatInput = document.getElementById('perguntatInput');
-const perguntaqInput = document.getElementById('perguntaqInput');
-const perguntacInput = document.getElementById('perguntacInput');
-const perguntasInput = document.getElementById('perguntasInput');
-const perguntaseInput = document.getElementById('perguntaseInput');
 const adcButton = document.getElementById('adcButton');
 const buttonSign = document.querySelector('button[name=adcButton]');
-
-adcButton.addEventListener('click', function () {
-    create(nameInput.value, dataInput.value, textInput.value, exemplosInput.value, perguntaInput.value, perguntadInput.value, perguntatInput.value, perguntatInput.value, perguntaqInput.value, perguntacInput.value, perguntasInput.value, perguntaseInput.value)
-});
 
 function create(nome, data, text, exemplos, pergunta, perguntad, perguntat, perguntaq, perguntac, perguntas, perguntase) {
     let user = {
@@ -33,4 +18,20 @@ function create(nome, data, text, exemplos, pergunta, perguntad, perguntat, perg
     return firebase.database().ref().child('questionario').push(user);
 }
 
-
+adcButton.addEventListener('click', function () {
+    const nameInput = document.getElementById('nomeEscola');
+    const dataInput = document.getElementById('dataDepoimento');
+    const textInputm = document.getElementById('textDepoimento');
+    const exemplosInput = document.getElementById('exemplosInput');
+    const perguntaInput = document.getElementById('perguntaInput');
+    const perguntadInput = document.getElementById('perguntadInput');
+    const perguntatInput = document.getElementById('perguntatInput');
+    const perguntaqInput = document.getElementById('perguntaqInput');
+    const perguntacInput = document.getElementById('perguntacInput');
+    const perguntasInput = document.getElementById('perguntasInput');
+    const perguntaseInput = document.getElementById('perguntaseInput');
+    
+    create(nameInput.value, dataInput.value, textInputm.value, exemplosInput.value, perguntaInput.value, perguntadInput.value, perguntatInput.value, perguntatInput.value, perguntaqInput.value, perguntacInput.value, perguntasInput.value, perguntaseInput.value);
+    alert('Muito obrigado!! Volte para página inicial.')
+   
+});
